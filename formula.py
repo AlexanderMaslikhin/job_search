5104ebdae12ae597410fc73d6bc61aa9d7ff8b2d
AlexanderMaslikhin <shu75@yandex.ru>
2023-01-05 19:35:43 +0300

    fee

--- formula.py
+++ formula.py
@@ -1,13 +1,13 @@
 from math import sqrt
 
 
-def func(x, pool_list, k_num=None, k_den_0=None, k_den_1=None):
+def func(x, pool_list, k_num=None, k_den_0=None, k_den_1=None, fee=0.997):
     if k_den_0 is None or k_den_1 is None or k_num is None:
-        return func(x, pool_list[1:], pool_list[0][1], pool_list[0][0], 1.)
+        return func(x, pool_list[1:], pool_list[0][1], pool_list[0][0], fee*1.)
     if len(pool_list) == 0:
-        return k_num, k_den_0, k_den_1, k_num*x/(k_den_0 + k_den_1*x)
+        return k_num, k_den_0, k_den_1, k_num*x/(k_den_0 + fee*k_den_1*x)
     pool0, pool1 = pool_list[0]
-    return func(x, pool_list[1:], k_num*pool1, k_den_0*pool0, pool0*k_den_1+k_num)
+    return func(x, pool_list[1:], k_num*pool1, k_den_0*pool0, fee*(pool0*k_den_1+k_num))
 
 
 def find_max(pool_list, f):
